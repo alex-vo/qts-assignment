039ca70cc296852556bbdc3a99d11a2a1105232a
Alexey Vorontsov <avoroncovs@gmail.com>
2024-12-01 13:19:38 +0200

    task2 done

--- binanceHistoricalData.ts
+++ binanceHistoricalData.ts
@@ -14,13 +14,21 @@ const toMillis = (interval: string): number => {
   throw new Error(`Unexpected interval ${interval}`);
 }
 
-// const {data: {symbols}} = await axios.get<BinanceExchangeInfo>('https://api.binance.com/api/v3/exchangeInfo?symbols=["ETHUSDT"]&showPermissionSets=false');
+const API_MAX_RESPONSE_LINES = 1500;
+const ticker = "DOGEUSDT";
 const interval = "1h";
+const intervalMillis = toMillis(interval);
 const now = new Date().getTime();
-const nowMinus150Days = now - 150 * 24 * 60 * 60 * 1000;
-const {data} = await axios.get(`https://fapi.binance.com/fapi/v1/klines?symbol=DOGEUSDT&interval=${interval}&startTime=${nowMinus150Days}&endTime=${now}&limit=1500`);
-
-console.log(data.length);
-console.log(new Date(data[0][0]));
-console.log(new Date(data[data.length - 1][0]));
-console.log((data[data.length - 1][0] - data[0][0]) / (1000 * 60 * 60));
+const historicalDataDays = 150;
+const requests = Math.ceil((historicalDataDays * 24 * 60 * 60 * 1000) / (toMillis(interval) * API_MAX_RESPONSE_LINES));
+const nowMinus150Days = now - historicalDataDays * 24 * 60 * 60 * 1000;
+const historicalData150Days = (await Promise.all([...Array(requests)]
+  .map((_, i) => nowMinus150Days + i * API_MAX_RESPONSE_LINES * intervalMillis)
+  .map(
+    async (startTime) => (
+      await axios.get(`https://fapi.binance.com/fapi/v1/klines?symbol=${ticker}&interval=${interval}&startTime=${startTime}&endTime=${now}&limit=1500`)
+    ).data
+  )
+)).flat();
+console.log(`Dataframe for ${ticker} perp future ${historicalDataDays} days historical candle data with 1 hour interval:`);
+console.log({ticker, historicalData: historicalData150Days});
--- binanceTickersDataFrame.ts
+++ binanceTickersDataFrame.ts
@@ -6,18 +6,19 @@ type BinanceExchangeInfo = {
   }[];
 };
 
-// const {data: {symbols}} = await axios.get<BinanceExchangeInfo>('https://api.binance.com/api/v3/exchangeInfo?symbols=["ETHUSDT"]&showPermissionSets=false');
-const {data: {symbols}} = await axios.get<BinanceExchangeInfo>("https://api.binance.com/api/v3/exchangeInfo?showPermissionSets=false&permissions=SPOT");
+const {data: {symbols}} = await axios
+  .get<BinanceExchangeInfo>("https://api.binance.com/api/v3/exchangeInfo?showPermissionSets=false&permissions=SPOT");
 
 const nowMinus20Min = new Date().getTime() - 20 * 60 * 1000;
 const data = await Promise.all(
   symbols.filter(({symbol}) => symbol.endsWith("USDT"))
     .map(async ({symbol}) => {
-        const {data: klinesData} = await axios.get(`https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=5m&startTime=${nowMinus20Min}`)
+        const {data: historicalData} = await axios
+          .get(`https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=5m&startTime=${nowMinus20Min}`)
 
-        return {ticker: symbol, klinesData};
+        return {ticker: symbol, historicalData};
       }
     )
 );
-console.log("Dataframe:");
+console.log("Dataframe for spot tickers traded in USDT with 5 minute interval historical candle data:");
 console.log(data);
